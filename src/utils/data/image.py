86c1d943bd9c4293f227a12141ab6cb3099436d3
Victor Velev <victorivelev@gmail.com>
2019-03-28 22:04:22 +0000

    train t

--- src/utils/data/image.py
+++ src/utils/data/image.py
@@ -8,11 +8,15 @@ __all__ = [
 
 
 def get_preprocessed_img(img_path, image_size=(256, 256)):
-    img = cv2.imread(img_path)
-    img = cv2.resize(img, image_size)
-    img = img.astype('float32')
-    img /= 255
-    return img
+    img_rgb = cv2.imread(img_path)
+    img_grey = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    img_adapted = cv2.adaptiveThreshold(img_grey, 255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY, 101, 9)
+    img_stacked = np.repeat(img_adapted[...,None],3,axis=2)
+    resized = cv2.resize(img_stacked, (200,200), interpolation=cv2.INTER_AREA)
+    bg_img = 255 * np.ones(shape=(256,256,3))
+    bg_img[27:227, 27:227, :] = resized
+    bg_img /= 255
+    return bg_img
 
 def show(img_path):
     image = cv2.imread(img_path)
