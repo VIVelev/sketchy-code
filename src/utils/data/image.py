5dce79e19c5042c8d80e8bb9d71306cc02318793
Victor Velev <velev.victor@yahoo.com>
2019-03-29 20:42:06 +0200

    [Fix] image loading

--- src/utils/data/image.py
+++ src/utils/data/image.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 __all__ = [
     'get_preprocessed_img',
@@ -9,14 +8,8 @@ __all__ = [
 
 def get_preprocessed_img(img_path, image_size=(256, 256)):
     img_rgb = cv2.imread(img_path)
-    img_grey = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-    img_adapted = cv2.adaptiveThreshold(img_grey, 255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY, 101, 9)
-    img_stacked = np.repeat(img_adapted[...,None],3,axis=2)
-    resized = cv2.resize(img_stacked, (200,200), interpolation=cv2.INTER_AREA)
-    bg_img = 255 * np.ones(shape=(256,256,3))
-    bg_img[27:227, 27:227, :] = resized
-    bg_img /= 255
-    return bg_img
+    img_rgb = cv2.resize(img_rgb, image_size)
+    return img_rgb
 
 def show(img_path):
     image = cv2.imread(img_path)
