d2145a8ab9cfcb6d0f8239a69647e8d5c099341d
Victor Velev <velev.victor@yahoo.com>
2019-03-31 07:14:27 +0300

    [Update] add drawn test

--- src/utils/data/image.py
+++ src/utils/data/image.py
@@ -8,9 +8,14 @@ __all__ = [
 
 def get_preprocessed_img(img_path, image_size=(256, 256)):
     img = cv2.imread(img_path)
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
+
     img = cv2.resize(img, image_size)
     img = img.astype('float32')
     img /= 255
+
     return img
 
 def show(img_path):
