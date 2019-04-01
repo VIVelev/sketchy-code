f6d6ec82212b3fcc99d83439168aae76150153d3
Victor Velev <velev.victor@yahoo.com>
2019-04-01 18:22:28 +0300

    [Updat-Doc] document Sketch Encoder

--- src/nn/sketch_encoder.py
+++ src/nn/sketch_encoder.py
@@ -11,42 +11,62 @@ __all__ = [
 
 
 class SketchEncoder:
+    """Sketch Encoder
+    
+    Sketch (Image) Enmbedding (Encoder) Model
+    
+    Parameters:
+    -----------
+    embedding_dim : integer, the dimension in which to embed the sketch image and the tokens.
+    name : string, the name of the model, optional
+    
+    """
 
     def __init__(self, embedding_dim, name='sketch_encoder'):
         self.embedding_dim = embedding_dim
         self.name = name
 
+        # Inputs
         self.image_input = Input(IMAGE_SIZE, name='image_input')
 
+        # Conv 32
         self.conv_32_1 = Conv2D(32, (3, 3), activation='relu', padding='valid', name='conv_32_1')
         self.conv_32_2 = Conv2D(32, (3, 3), activation='relu', padding='valid', name='conv_32_2')
         self.maxpool_1 = MaxPool2D(pool_size=(2, 2), name='maxpool_1')
         self.conv_dropout_1 = Dropout(0.3, name='conv_dropout_1')
 
+        # Conv 64
         self.conv_64_1 = Conv2D(64, (3, 3), activation='relu', padding='valid', name='conv_64_1')
         self.conv_64_2 = Conv2D(64, (3, 3), activation='relu', padding='valid', name='conv_64_2')
         self.maxpool_2 = MaxPool2D(pool_size=(2, 2), name='maxpool_2')
         self.conv_dropout_2 = Dropout(0.3, name='conv_dropout_2')
-        
+
+        # Conv 128
         self.conv_128_1 = Conv2D(128, (3, 3), activation='relu', padding='valid', name='conv_128_1')
         self.conv_128_2 = Conv2D(128, (3, 3), activation='relu', padding='valid', name='conv_128_2')
         self.maxpool_3 = MaxPool2D(pool_size=(2, 2), name='maxpool_3')
         self.conv_dropout_3 = Dropout(0.3, name='conv_dropout_3')
 
+        # Flatten
         self.flatten = Flatten(name='flatten')
 
+        # Dense -> ReLU 1
         self.dense_relu_1 = Dense(1024, activation='relu', name='dense_relu_1')
         self.dense_dropout_1 = Dropout(0.3, name='dense_dropout_1')
 
+        # Dense -> ReLU 2
         self.dense_relu_2 = Dense(1024, activation='relu', name='dense_relu_2')
         self.dense_dropout_2 = Dropout(0.3, name='dense_dropout_2')
 
+        # Dense -> ReLU encoder
         self.dense_relu_encoder = Dense(embedding_dim, activation='relu', name='dense_relu_encoder')
         self.embedding_reshapor = Reshape((1, embedding_dim), name='embedding_reshapor')
 
         self.model = None
 
     def build_model(self):
+        """Builds a Keras Model to train/predict"""
+
         x = self.conv_32_1(self.image_input)
         x = self.conv_32_2(x)
         x = self.maxpool_1(x)
