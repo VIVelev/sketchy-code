15794c3dd87a0ff174e775125b226a05e9121f9e
Victor Velev <velev.victor@yahoo.com>
2019-04-04 22:25:02 +0300

    add some html data

--- src/utils/config.py
+++ src/utils/config.py
@@ -2,15 +2,15 @@ import glob
 
 __all__ = [
     'PATH_TO_DATA',
-    'GUIS_CODE',
-    'GUIS_SKETCH',
+    'HTML_CODE',
+    'SKETCH_IMG',
     'IMAGE_SIZE',
 ]
 
 
-PATH_TO_DATA = '/Users/victor/Desktop/sketchy-code/data/all_data/'
-GUIS_CODE = glob.glob(PATH_TO_DATA + '*.gui')
-GUIS_SKETCH = glob.glob(PATH_TO_DATA + '*.png')
+PATH_TO_DATA = '/Users/victor/Desktop/sketchy-code/data/sketches/'
+HTML_CODE = glob.glob(PATH_TO_DATA + '*.html')
+SKETCH_IMG = glob.glob(PATH_TO_DATA + '*.png')
 IMAGE_SIZE = (256, 256, 3)
 
-assert len(GUIS_CODE) == len(GUIS_SKETCH)
+# assert len(HTML_CODE) == len(SKETCH_IMG)
--- src/utils/sequence.py
+++ src/utils/sequence.py
@@ -1,5 +1,6 @@
 __all__ = [
     'tokenize_dsl_code',
+    'tokenize_html_code',
 ]
 
 
@@ -15,3 +16,20 @@ def tokenize_dsl_code(code):
             final_tokens.append(token)
 
     return final_tokens
+
+def tokenize_html_code(code):
+    html_tags = []
+    closing_bound = 0
+    
+    while True:
+        closing_idx = code.find('>', closing_bound)
+        if closing_idx == -1:
+            break
+
+        tag = code[closing_bound:closing_idx+1]
+        tag = tag[tag.find('<'):tag.find('>')+1]
+
+        html_tags.append(tag)
+        closing_bound = closing_idx+1
+
+    return html_tags
