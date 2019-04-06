77d7f3db9ea471e43277a7dbeb15af85110d783b
Victor Velev <velev.victor@yahoo.com>
2019-04-06 20:11:39 +0300

    [Update] rename all_data -> toy_dataset (more appropriate name)

--- data/get_data.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-wget http://sketch-code.s3.amazonaws.com/data/all_data.zip -O ./all_data.zip
-unzip ./all_data.zip -d ./all_data
---
+++ data/get_toy_dataset.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+wget http://sketch-code.s3.amazonaws.com/data/all_data.zip -O ./toy_dataset.zip
+unzip ./toy_dataset.zip -d ./toy_dataset
